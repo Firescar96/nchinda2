a035b0618509b26172aca5977e0f9ca0ead2d97f
Nchinda Nchinda <nchinda2@gmail.com>
2019-07-14 12:37:47 -0400

    progress on firescar opener

--- src/components/Firescar.vue
+++ src/components/Firescar.vue
@@ -1,6 +1,12 @@
 <template>
   <div id="firescarPage">
     <div>Firescar96</div>
+    <div id="lavalamp">
+      <div class="fire">
+        <div v-for="ball in lavaballs" :key="ball" class="burn" />
+        <div v-for="ball in lavaballs" :key="ball" class="burn heat" />
+      </div>
+    </div>
 
     <div v-for="(post, index) in posts" :key="index" class="postSummary">
       <div class="link" @click="selectedPost=post" v-html="post.title" />
@@ -30,11 +36,8 @@ class Welcome {
     return {
       posts: [],
       selectedPost: null,
+      lavaballs: new Array(30),
     };
-  }
-
-  mounted() {
-    //lavalamp wallpaper
   }
 
   async created() {
@@ -59,8 +62,89 @@ class Welcome {
 
 #firescarPage {
   font-family: VT323;
+  background: #000;
+
+  $fireSize: 800;
+  $burnSize: 200;
+  $burnCount: 30;
+
+  #lavalamp {
+    position: absolute;
+    top: 50%;
+    right: 0;
+    margin-top: #{-$fireSize / 4}px;
+    transform: translateY(-50%);
+
+    .fire {
+      position: relative;
+      width: #{$fireSize}px;
+      height: #{$fireSize}px;
+      background: #ff9900;
+      filter: blur(20px) contrast(5);
+      border: #{$fireSize/2}px solid #000;
+      border-bottom-color: transparent;
+      border-radius: 40%;
+      box-sizing: border-box;
+      transform: scale(0.4, 1);
+      animation: lavashade 10s infinite cubic-bezier(0.42, 0.0, 0.58, 1.0);
+
+      .burn {
+        position: absolute;
+        top: #{$fireSize}px;
+        left: #{-$burnSize / 2}px;
+        width: #{$burnSize}px;
+        height: #{$burnSize}px;
+        background: #000;
+        border-radius: 100%;
+
+        @for $i from 1 through $burnCount * 2 {
+          &.heat:nth-child(#{$i}) {
+            height: #{random(50)}px;
+          }
+          &:nth-child(#{$i}) {
+            height: #{random($burnSize / 2)}px;
+            margin-left: #{random($fireSize) - $fireSize / 2}px;
+            animation: burning #{random(5000) + 12000}ms infinite linear;
+          }
+        }
+      }
+    }
+  }
+
+  @keyframes burning {
+    0% {
+      transform: translateY(-300px);
+    }
+    100% {
+      transform: translateY(-#{$fireSize+$burnSize}px);
+    }
+  }
+
+  @keyframes lavashade {
+    0% {
+      background: #ff9900;
+    }
+    5% {
+      background: #ff9900;
+    }
+    45% {
+      background: #9600ff;
+    }
+    55% {
+      background: #9600ff;
+    }
+    95% {
+      background: #ff9900;
+    }
+    100% {
+      background: #ff9900;
+    }
+  }
 
   .postSummary {
+    padding-left: 20px;
+    color: white;
+    width: 500px;
     text-align: left;
   }
 }
