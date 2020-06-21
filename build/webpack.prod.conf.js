fc773a03607c0df783c014edc3ec0b06e95bcfd6
Nchinda Nchinda <nchinda2@gmail.com>
2020-06-21 00:48:34 -0400

    update dependencies

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -74,13 +74,15 @@ const webpackConfig = merge(baseWebpackConfig, {
     // enable scope hoisting
     new webpack.optimize.ModuleConcatenationPlugin(),
     // copy custom static assets
-    new CopyWebpackPlugin([
-      {
-        from: path.resolve(__dirname, '../static'),
-        to: config.build.assetsSubDirectory,
-        ignore: ['.*']
-      }
-    ]),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, '../static'),
+          to: config.dev.assetsSubDirectory,
+          globOptions: { ignore: ['.*'] }
+        }
+      ]
+    }),
     new MiniCssExtractPlugin({
       filename: utils.assetsPath('css/[name].css'),
       chunkFilename: utils.assetsPath('[name].[id].css')
